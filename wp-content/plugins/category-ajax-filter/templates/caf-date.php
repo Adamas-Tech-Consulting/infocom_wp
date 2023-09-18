0651a23f57a692cd455915cb3894c0d61a17bc5e
biswajeetsen <biswajeet1.sen@adamastech.in>
2023-09-18 17:57:15 +0530

    change-new2

--- wp-content/plugins/category-ajax-filter/templates/caf-date.php
+++ wp-content/plugins/category-ajax-filter/templates/caf-date.php
@@ -6,5 +6,5 @@ $caf_post_date_format = apply_filters('tc_caf_post_date_format', $caf_post_date_
             if ($caf_post_layout == 'post-layout1' || $caf_post_layout == 'post-layout11') {
                 echo "<span class='date caf-col-md-6 caf-pl-0'><i class='fa fa-calendar' aria-hidden='true'></i> 111" . get_the_date($caf_post_date_format) . "</span>";
             } else {
-                echo "<span class='date caf-pl-0'><i class='fa fa-calendar' aria-hidden='true'></i> " 222. get_the_date($caf_post_date_format) . "</span>";
+                echo "<span class='date caf-pl-0'><i class='fa fa-calendar' aria-hidden='true'></i> 222". get_the_date($caf_post_date_format) . "</span>";
             }
