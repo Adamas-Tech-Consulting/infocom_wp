1ca8bdc057782348b0b705052055305e8791debb
biswajeetsen <biswajeet1.sen@adamastech.in>
2023-10-17 16:31:30 +0530

    aa

--- wp-content/themes/indiainfocom-child/single-ciolist.php
+++ wp-content/themes/indiainfocom-child/single-ciolist.php
@@ -9,44 +9,45 @@
  * @since Twenty Twenty-One 1.0
  */
 
-get_header();
+get_header(); ?>
 
+<div class="cio-banner position-relative">
+	<?php the_post_thumbnail(); ?>
+	<div class="container negative-space">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="d-flex">
+					<div class="flex-shrink-0">
+					<img src="<?php the_field('add_profile_picture');?>" class="img-fluid" />
+					</div>
+					<div class="flex-grow-1 ms-3 blog-cover">
+						<span><?php the_field('add_designation');?></span>
+						<strong><?php the_field('add_company_name');?></strong>
+						<em><?php the_field('add_location');?></em>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<?php
 /* Start the Loop */
 while ( have_posts() ) :
-	the_post();
+	the_post(); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="cio-content">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
 
-	// get_template_part( 'template-parts/content/content-single' );
 
-	echo 'This is cio wall single page';
-
-	if ( is_attachment() ) {
-		// Parent post navigation.
-		the_post_navigation(
-			array(
-				/* translators: %s: Parent post link. */
-				'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'twentytwentyone' ), '%title' ),
-			)
-		);
-	}
-
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-
-	// Previous/next post navigation.
-	$twentytwentyone_next = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' );
-	$twentytwentyone_prev = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' );
-
-	$twentytwentyone_next_label     = esc_html__( 'Next post', 'twentytwentyone' );
-	$twentytwentyone_previous_label = esc_html__( 'Previous post', 'twentytwentyone' );
-
-	the_post_navigation(
-		array(
-			'next_text' => '<p class="meta-nav">' . $twentytwentyone_next_label . $twentytwentyone_next . '</p><p class="post-title">%title</p>',
-			'prev_text' => '<p class="meta-nav">' . $twentytwentyone_prev . $twentytwentyone_previous_label . '</p><p class="post-title">%title</p>',
-		)
-	);
+<?php	
 endwhile; // End of the loop.
 
 get_footer();
