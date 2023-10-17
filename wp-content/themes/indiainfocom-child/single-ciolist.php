c2c47073bb359312ec711941d0446d3c91d3eca9
biswajeetsen <biswajeet1.sen@adamastech.in>
2023-10-17 17:34:06 +0530

    aaa

--- wp-content/themes/indiainfocom-child/single-ciolist.php
+++ wp-content/themes/indiainfocom-child/single-ciolist.php
@@ -13,17 +13,20 @@ get_header(); ?>
 
 <div class="cio-banner position-relative">
 	<?php the_post_thumbnail(); ?>
-	<div class="container negative-space">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="d-flex">
-					<div class="flex-shrink-0">
-					<img src="<?php the_field('add_profile_picture');?>" class="img-fluid" />
-					</div>
-					<div class="flex-grow-1 ms-3 blog-cover">
-						<span><?php the_field('add_designation');?></span>
-						<strong><?php the_field('add_company_name');?></strong>
-						<em><?php the_field('add_location');?></em>
+	<div class="container">
+		<div class="negative-space">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="d-flex">
+						<div class="flex-shrink-0">
+							<img src="<?php the_field('add_profile_picture');?>" class="img-fluid" />
+						</div>
+						<div class="flex-grow-1 blog-cover p-4">
+							<h3><?php the_title(); ?></h3>
+							<span><?php the_field('add_designation');?></span>
+							<strong><?php the_field('add_company_name');?></strong>
+							<em><?php the_field('add_location');?></em>
+						</div>
 					</div>
 				</div>
 			</div>
